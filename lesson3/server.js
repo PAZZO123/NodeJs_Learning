6c04471d11a1e0064f51c1363fbeb1213e213a74
PAZZO123 <smbabazipatrick@gmail.com>
2026-04-30 18:32:08 +0200

    Adding some packages

--- lesson3/server.js
+++ lesson3/server.js
@@ -1,15 +1,47 @@
 const http=require('http')
 const fs=require('fs')
+const _=require('lodash')
 const server=http.createServer((req, res )=>{
-    console.log(req)
+    //lodash
+    const num=_.random(0,20)
+    const greet=_.once(()=>{
+        console.log('Hello Patrick .')
+    })
+    console.log(num)
+     greet()
+     greet()
     res.setHeader('Content-Type','text/html');
-    fs.readFile('./lesson3/index.html',(err,data)=>{
+    //Routing mechanism
+    let path='./lesson3/'
+    switch(req.url){
+        case '/':
+            path +='index.html'
+            // Setting status Codes
+            res.statusCode=200
+            break;
+        case '/about':
+            path +='about.html'
+            res.statusCode=200
+            break;
+        case '/about-us':
+            res.statusCode=301
+            //Redirects in nodejs
+              res.setHeader('Location', '/about')
+              res.end()
+            break;
+        default:
+            path +='404page.html' 
+            res.statusCode=404
+
+            break;
+    }
+    fs.readFile(path,(err,data)=>{
         if(err){
             console.log(err)
             res.end()
         }else{
-            res.write(data)
-            res.end()
+           // res.write(data)
+            res.end(data)
         }
     })
 });

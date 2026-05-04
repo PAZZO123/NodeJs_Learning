ca0a3bd0544d821a02babff8a61ae3a53e242480
PAZZO123 <smbabazipatrick@gmail.com>
2026-05-04 17:47:03 +0200

    Adding..

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express =require('express')
+const path=require('path')
 
 //express app
 const app =express()
@@ -7,8 +8,10 @@ app.listen(3000);
 
 app.get('/', (req,res)=>{
     //infer the content and set the content headers automtically and status code
-    res.send('<p> Hello from Express</p>')
+    //res.send('<p> Hello from Express</p>')
+    res.sendFile(path.join(__dirname,'lesson3','index.html'))
 })
 app.get('/about', (req,res)=>{
-    res.send('<p> Hello from Express This is about page!</p>')
+    //res.send('<p> Hello from Express This is about page!</p>')
+    res.sendFile(path.join(__dirname,'lesson3','about.html'))
 })

29a95202d750a805e0e4714d1935a24426dcc8ce
PAZZO123 <smbabazipatrick@gmail.com>
2026-05-04 19:52:54 +0200

    adding...

--- app.js
+++ app.js
@@ -13,5 +13,13 @@ app.get('/', (req,res)=>{
 })
 app.get('/about', (req,res)=>{
     //res.send('<p> Hello from Express This is about page!</p>')
+    //res.sendFile("./lesson3/about.html",{root:__dirname})
     res.sendFile(path.join(__dirname,'lesson3','about.html'))
+})
+app.get('/about-us', (req,res)=>{
+    res.redirect('/about')
+})
+//.use to create middleware and this is the 404 page
+app.use((req, res)=>{
+    res.sendFile('/lesson3/404page.html',{root:__dirname})
 })

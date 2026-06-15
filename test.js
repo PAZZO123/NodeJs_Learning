21c8bf531a2b394d6d4410950fce10415bc9ee03
Straton Patrick Mbabazi <154052094+PAZZO123@users.noreply.github.com>
2026-06-15 17:05:39 +0200

    Remove database URL from test.js
    Removed sensitive database connection string.

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-const dbURL = 'mongodb+srv://ijpazzo:ijpazzo@cluster0.d4ltffq.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(dbURL)
 .then(() => {
